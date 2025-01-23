edb39d3a577532f4cb583c9f7d4f533e4cc4899a
Rohithumesh1 <118875932+Rohithumesh1@users.noreply.github.com>
2025-01-23 13:23:27 +0530

    Add files via upload

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,246 @@
+import 'package:flash_cart/pages/payment.dart';
+
+import 'user_details.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'cart_model.dart';
+import 'home_page.dart';
+import 'categories.dart';
+import 'package:flash_cart/pages/account_bottom_navigation_bar_item.dart' as AccountItem;
+
+
+import 'package:flash_cart/pages/payment.dart'; // Import the PaymentPage
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Colors.grey[800],
+        ),
+      ),
+      body: Consumer<CartModel>(
+        builder: (context, value, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  "My Cart",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: ListView.builder(
+                    itemCount: value.cartItems.length,
+                    padding: EdgeInsets.all(12),
+                    itemBuilder: (context, index) {
+                      final item = value.cartItems[index];
+                      final totalPrice = double.parse(item[1].toString()) *
+                          value.getQuantityAtIndex(index);
+                      return Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Stack(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  borderRadius: BorderRadius.circular(8)),
+                              child: ListTile(
+                                leading: Image.asset(
+                                  item[2],
+                                  height: 36,
+                                ),
+                                title: Text(
+                                  item[0],
+                                  style: const TextStyle(fontSize: 18),
+                                ),
+                                subtitle: Row(
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(Icons.remove),
+                                      onPressed: () {
+                                        value.decreaseQuantityAtIndex(index);
+                                      },
+                                    ),
+                                    Text(
+                                      '${value.getQuantityAtIndex(index)}',
+                                      style: const TextStyle(fontSize: 12),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.add),
+                                      onPressed: () {
+                                        value.increaseQuantityAtIndex(index);
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: IconButton(
+                                icon: const Icon(Icons.cancel),
+                                onPressed: () =>
+                                    Provider.of<CartModel>(
+                                        context, listen: false)
+                                        .removeItemFromCart(index),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  '\₹${totalPrice.toStringAsFixed(2)}',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.green,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.green,
+                  ),
+                  padding: const EdgeInsets.all(24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Total Price',
+                            style: TextStyle(color: Colors.green[200]),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            '\₹${value.calculateTotal()}',
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PaymentInit(
+
+                            amount: double.parse(value.calculateTotal()),
+                             // Pass the total amount to PaymentInit
+                              metroCard: '123456789', // Example metro card number
+                              passedVals: {}, // Example passed values
+                              source: 'SmartCard', // Example source
+                            )),
+                          );
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.green.shade200),
+                            borderRadius: BorderRadius.circular(28),
+                          ),
+                          padding: const EdgeInsets.all(12),
+                          child: Row(
+                            children: const [
+                              Text(
+                                'Pay Now',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                size: 16,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: AccountItem.AccountBottomNavigationBarItem(), // Use AccountBottomNavigationBarItem
+            label: 'Account',
+          ),
+        ],
+        selectedItemColor: Color(0xff6c63ff),
+        unselectedItemColor: Colors.grey,
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HomePage()),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Categories()),
+              );
+              break;
+            case 2:
+              break;
+            case 3:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => UserDetails()), // Navigate to UserDetails
+              );
+              break;
+          }
+        },
+      ),
+    );
+  }
+}
